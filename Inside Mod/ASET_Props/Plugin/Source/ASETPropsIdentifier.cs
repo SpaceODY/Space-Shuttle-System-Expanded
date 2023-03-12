806b5b0e975fdb042df16b2cd770c0e8f57df72b
TreeMaster <102701854+You548@users.noreply.github.com>
2023-03-12 10:13:36 +0900

    Add files via upload

---
+++ Inside Mod/ASET_Props/Plugin/Source/ASETPropsIdentifier.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle ("ASETPropsIdentifier")]
+[assembly: AssemblyCopyright ("Mihara, released into public domain.")]
+// This is the version number that is used for linking.
+[assembly: AssemblyVersion ("1.4.0.0")]
+// This is the version number ModuleManager and the user sees.
+[assembly: AssemblyFileVersion ("1.4.0.0")]
+
+namespace ASETPropsIdentifier
+{
+	// This is the source code of this plugin.
+	// No, really, this is it.
+	// It's only purpose is to contain a version number that ModuleManager can detect,
+	// so that patches can be applied as appropriate.
+}
+
